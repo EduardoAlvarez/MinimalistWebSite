8677d5129e064c138f191c2eebc4db8e3aa4e212
Eduardo Alvarez <eduardo.alvarez1@hotmail.com>
2015-12-03 21:01:10 -0200

    Efeito muito show no texto com a borda

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,47 @@ $( document ).ready(function(){
 
 	var boxes = $(".second-line .box");
 	boxes.click(function(){
+		
+		if($(".resumo-bg").width() != 0 ){
+			
+			$( ".resumo-bg" ).animate({
+			    width: 0,
+			}, 500, function() {
+				
+			});
+
+
+			$( ".resumo-text-2").animate({
+			    width: 0,
+			}, 500, function() {
+				
+			});
+			
+
+		}else{
+			$(".resumo-text-2").show();
+			$( ".resumo-bg " ).animate({
+			    width: "100%",
+			}, 500, function() {
+				
+			});
+			$( ".resumo-text-2").animate({
+			    width: "100%",
+			}, 500, function() {
+				
+			});
+
+		}
+
+		 
+		
+
 		boxes.css('opacity' , '0.4');
 		var _self = $(this);
 		_self.css("opacity" , '1');
-		var texto = $("#"+_self.attr('rel'));
-		$(".sky .categoria").each(function(){
-			if($(this).attr('id') != _self.attr("rel") && $(this).is(':visible')){
-				texto.attr('slow' , "true");
-				$(this).slideUp('slow',function(){
-					texto.slideDown();
-				});		
-			}
-		});
-		if(texto.attr("slow") != "true"){
-			texto.slideDown();
-		}else{
-			texto.attr("slow" , "false");
-		}
-		console.log(texto.attr("slow"));
+		
+
+
 	});
 });
 

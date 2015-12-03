655068eca37612fe0b4930d4b7b7bbcedce9e93c
Eduardo Alvarez <eduardo.alvarez1@hotmail.com>
2015-12-03 21:17:03 -0200

    Animação redinada e click fora volta ao inicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,55 @@
 $( document ).ready(function(){
 
 	var boxes = $(".second-line .box");
-	boxes.click(function(){
-		
-		if($(".resumo-bg").width() != 0 ){
-			
-			$( ".resumo-bg" ).animate({
-			    width: 0,
-			}, 500, function() {
-				
-			});
-
-
-			$( ".resumo-text-2").animate({
-			    width: 0,
-			}, 500, function() {
-				
-			});
-			
-
-		}else{
-			$(".resumo-text-2").show();
-			$( ".resumo-bg " ).animate({
-			    width: "100%",
-			}, 500, function() {
-				
-			});
-			$( ".resumo-text-2").animate({
-			    width: "100%",
-			}, 500, function() {
-				
-			});
-
-		}
-
-		 
-		
-
-		boxes.css('opacity' , '0.4');
+	boxes.click(function(){		
 		var _self = $(this);
+		doAnimation(_self);
+		boxes.css('opacity' , '0.4');
 		_self.css("opacity" , '1');
-		
+	});
 
-
+	$("body").click(function(e){
+		if( $(e.target).hasClass('content')  == true ){
+			doAnimation($(this));
+		}
 	});
 });
 
- 
+function doAnimation(_self){
+	var id = _self.attr('id');
+	var text = $("#"+id+"-content").html();
+	if($(".resumo-bg").width() != 0 ){
+		whiteAnimation();
+		$(".resumo-text").html(text);
+	}else{
+		blackAnimation();
+		$(".resumo-text-2").html(text);
+	}
+}
+
+function whiteAnimation() {
+ 	$( ".resumo-bg" ).animate({
+	    width: 0,
+	}, 500, function() {
+		
+	});
+	$( ".resumo-text-2").animate({
+	    width: 0,
+	}, 500, function() {
+		
+	});
+ }
+
+function blackAnimation() {
+	$(".resumo-text-2").show();
+	$( ".resumo-bg " ).animate({
+	    width: "100%",
+	}, 500, function() {
+		
+	});
+	$( ".resumo-text-2").animate({
+	    width: "100%",
+	}, 500, function() {
+		
+	});
+ }

ee716deea46b4c6ca888796ffd5416d137a21f24
Eduardo Alvarez <eduardo.alvarez1@hotmail.com>
2015-12-10 23:17:43 -0200

    Alterando conforme muda a janela do navegador

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,9 +1,11 @@
 $( document ).ready(function(){
-	var mq = window.matchMedia( "(max-width: 800px)" );
-	if(mq.matches){
-		var height = window.innerHeight;
-		$('.content').css('height' , height+"px")
-	}
+	$( window ).resize(function() {
+		var mq = window.matchMedia( "(max-width: 800px)" );
+		if(mq.matches){
+			var height = window.innerHeight;
+			$('.content').css('height' , height+"px")
+		}
+	});
 
 	
 });

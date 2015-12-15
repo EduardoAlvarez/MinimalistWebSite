650edf99714bed3da74cc2ad309f77f918e261fd
Eduardo Alvarez <eduardo.alvarez1@hotmail.com>
2015-12-14 23:46:08 -0200

    Mobile fix

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,11 +1,14 @@
 $( document ).ready(function(){
 	$( window ).resize(function() {
+		resize();
+	});
+	resize();
+	function resize(){
 		var mq = window.matchMedia( "(max-width: 800px)" );
 		if(mq.matches){
 			var height = window.innerHeight;
 			$('.content').css('height' , height+"px")
 		}
-	});
+	}
 
-	
 });
